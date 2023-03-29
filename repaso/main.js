cd77a0a3936ebd94c5abf96a6ae8ee6a144b849e
dayana <dayana.romero88@gmail.com>
2023-03-29 13:58:32 +0200

    repaso

---
+++ repaso/main.js
@@ -0,0 +1,11 @@
+const tasks = [
+  { name: 'Viaje en yate por el mediterráneo', completed: true },
+  { name: 'Ganarme la lotería', completed: true },
+  { name: 'Saltar en paracaídas', completed: true },
+  { name: 'Practicar JavaScript', completed: false },
+];
+
+//pintar en el HTML un checkbox dentro de un li por  cada tarea,
+//  Y marcar la tarea como hecha ponerle una clase subrayado
+
+console.log('ha cargado');
